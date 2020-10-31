687062b95f5fa8dc939e24164f3ac4170f3f80aa
matheus-debug <matheussantanaengineer@gmail.com>
2020-10-30 21:17:43 -0300

    Correção

--- public/scripts/page-orfanatos.js
+++ public/scripts/page-orfanatos.js
@@ -1,25 +1,29 @@
+//create map
 const map = L.map('mapid').setView([-24.1591177,-46.7977846], 15);
 //create and add tiereyer
 
 
-L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
+L.tileLayer( 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', )
+.addTo(map);
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [50, 68],
+    iconUrl: "../public/images/map-marker.svg",
+    iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 })
 
-//create popup
+//create popup overlay
 const popup = L.popup({
     closeButton: false,
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar Das meninas <a href="orfanatos.html?id=1" class="escoolha-orfanato" <img  src=".public/images/arrow-white.svg"></a>')
+ }).setContent('Lar Das meninas <a href="orfanato.html?id=1" class="escoolha-orfanato"> <img  src=".public/images/arrow-white.svg"> </a>')
 
-L.marker([-24.1591177,-46.7977846], {icon})
+//create and add marker
+L
+    .marker([-24.1591177,-46.7977846], {icon: icon})
     .addTo(map)
-    .bindPopup('Essa é a casa do Matheus')
+    .bindPopup('Lar das meninas')
