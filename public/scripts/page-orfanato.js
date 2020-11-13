96af8ba7847e51536669678ef5500baf43f3192b
matheus-debug <matheussantanaengineer@gmail.com>
2020-11-12 21:28:11 -0300

    Atualização

--- public/scripts/page-orfanato.js
+++ public/scripts/page-orfanato.js
@@ -5,8 +5,8 @@ const options = {
   touchZoom: false,
   doubleClickZoom: false,
   scrollWheelZoom: false,
-  zoomControl: false
-}
+  zoomControl: false,
+};
 
 //create map
 const map = L.map("mapid", options).setView([-24.1591177, -46.7977846], 15);
@@ -22,7 +22,26 @@ const icon = L.icon({
   popupAnchor: [170, 2],
 });
 
-
 //create and add marker
-L.marker([-24.1591177, -46.7977846], { icon: icon })
-  .addTo(map)
+L.marker([-24.1591177, -46.7977846], { icon: icon }).addTo(map);
+
+/*IMAGE GALLERY*/
+
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  //remover todas as classes active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach(removeActiveClass);
+
+  function removeActiveClass(button) {
+    button.classList.remove("active");
+  }
+  //selecionar a imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orfanato-detalhes > img");
+  //atualizar o container de imagem
+  imageContainer.src = image.src;
+  //adicionar a classe active
+  button.classList.add("active");
+}
