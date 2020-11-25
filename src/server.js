955aeb842ed0efbfcd84dbab8e63101a18b628b4
matheus-debug <matheussantanaengineer@gmail.com>
2020-11-25 20:39:31 -0300

    Instalando bibliotecas

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('Teste Terminal')
+
+//Importar a biblioteca
+const express = require('express');
+const path = require('path')
+// Iniciando a biblioteca express
+const server = express()
+
+server
+
+//utilizando os arquivos estaticos
+.use(express.static('public'))
+//Criar uma rota
+//O que estiver entre '/' indica a rota da minha página que desejo acessar
+.get('/', (request, response) => {
+ return response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+//Ligar o servidor
+server.listen(5500)
